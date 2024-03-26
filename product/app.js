b063c4e12809fc4424b1b97a4cd151b48c693565
NishaVyas <96906856+NishaVyas@users.noreply.github.com>
2024-03-26 15:44:32 +0530

    By Routes

--- product/app.js
+++ product/app.js
@@ -1,20 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/add-product', (req,res,next) => {
-    console.log("In another middleware");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add product</button></form>')
+app.use(adminRoutes);
+app.use(shopRoutes);
+app.use((req,res,next) => {
+    res.status(404).send('<h1>Page not found</h1>');
 });
-
-app.post('/product', (req,res,next) =>{
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/', (req,res,next)=>{
-    res.send('<h1>Hello From Express!</h1>')
-})
-
 app.listen(3000);
